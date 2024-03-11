90b5cbdb6401279fc45e22885cac3a29356ae643
geod <geodgit@duck.com>
2024-03-11 22:10:22 +0100

    Polls display and voting

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {RefreshControl, StyleSheet, Text, View} from 'react-native';
+import {useEffect, useState} from "react";
+import {Endpoints} from "./Utils/Endpoints";
+import {Colors} from "./Utils/Colors";
+import Poll from "./Components/Poll";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [polls, setPolls] = useState([])
+	const [refreshing, setRefreshing] = useState(false);
+
+	const fetchPolls = async () => {
+		const res = await fetch(Endpoints.listPolls)
+		if (res.ok) {
+			const json = await res.json()
+			setPolls(json["hydra:member"])
+		}
+	}
+
+	const onRefresh = async () => {
+		setRefreshing(true);
+		await fetchPolls()
+		setRefreshing(false)
+	};
+
+	useEffect(() => {
+		fetchPolls()
+	}, [])
+
+	return (
+		<View style={styles.container} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
+			<StatusBar style="auto"/>
+			{polls ? polls.map(poll => <Poll poll={poll} key={poll.id}/>) : <Text>Loading</Text>}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		backgroundColor: Colors.surface,
+		color: Colors.onSurface,
+		flex: 1,
+		alignItems: 'center',
+		gap: 25,
+		paddingVertical: 50,
+	}
 });
---
+++ Components/Poll.js
@@ -0,0 +1,87 @@
+import {Button, StyleSheet, Text, View} from "react-native";
+import {Colors} from "../Utils/Colors";
+import {useEffect, useState} from "react";
+import {Storage} from "../Utils/Storage";
+import {Endpoints} from "../Utils/Endpoints";
+
+const Poll = ({poll}) => {
+	const [vote, setVote] = useState()
+	const [isLoading, setIsLoading] = useState(false)
+
+	const checkVote = async () => setVote(await Storage.get("poll-" + poll.id))
+
+	const addVote = async (index) => {
+		setIsLoading(true)
+		try {
+			const votes = poll.votes
+			votes[index] += 1
+			const res = await fetch(Endpoints.vote(poll.id), {
+				method: "PATCH",
+				body: JSON.stringify({votes: votes}),
+				headers: {
+					"Content-Type": "application/merge-patch+json",
+				}
+			})
+			if (res.ok) {
+				await Storage.set("poll-" + poll.id, index.toString())
+				setVote(index)
+			}
+		} catch (e) {
+			console.log(e)
+		}
+		setIsLoading(false)
+	}
+
+	useEffect(() => {
+		checkVote()
+	}, [])
+
+	return (
+		<View key={poll.id} style={styles.questionContainer}>
+			<Text style={styles.question}>{poll.question}</Text>
+			{!isLoading && vote !== undefined && poll.options.map((option, i) => vote ? (
+				<View key={i} style={{...styles.vote, ...(i === vote ? styles.voted : styles.unvoted)}}>
+					<Text>{option}</Text>
+					<Text>{poll.votes[i]}</Text>
+				</View>
+			) : (
+				<Button key={i} title={option} color={Colors.primary} onPress={() => addVote(i)}/>
+			))}
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	questionContainer: {
+		padding: 10,
+		borderColor: Colors.outline,
+		borderWidth: 1,
+		borderRadius: 10,
+		width: "90%",
+		display: "flex",
+		flexDirection: "column",
+		gap: 10,
+	},
+	question: {
+		fontSize: 15,
+		fontWeight: "bold"
+	},
+	vote: {
+		width: "100%",
+		padding: 10,
+		display: "flex",
+		flexDirection: "row",
+		alignItems: "center",
+		justifyContent: "space-between",
+		borderRadius: 5,
+	},
+	unvoted: {
+		borderColor: Colors.outline + "AA",
+		borderWidth: 1,
+	},
+	voted: {
+		backgroundColor: Colors.primary + "55"
+	}
+})
+
+export default Poll
