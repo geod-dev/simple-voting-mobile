23997a5f0abf2478d3d6480b7e549d58329c3082
geod <geodgit@duck.com>
2024-03-11 22:08:10 +0100

    Import project utils: colors, endpoints, secure storage

---
+++ Utils/Colors.js
@@ -0,0 +1,7 @@
+export class Colors {
+	static primary = '#3567d2';
+	static onPrimary = '#f5f5f5';
+	static outline = '#868686';
+	static surface = '#f5f5f5';
+	static onSurface = '#1c1c1c';
+}
---
+++ Utils/Endpoints.js
@@ -0,0 +1,5 @@
+export class Endpoints {
+	static api = "https://pure-forest-56731-05910aa4d1e9.herokuapp.com/api"
+	static listPolls = this.api + "/polls"
+	static vote = (id) => this.api + "/polls/" + id
+}
